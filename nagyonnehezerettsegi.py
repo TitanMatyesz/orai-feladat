52da45946f5f412f9d78fd41cff9579043cb3fac
Titán Matyesz <116081262+TitanMatyesz@users.noreply.github.com>
2024-02-28 08:31:28 +0100

    Add files via upload

---
+++ nagyonnehezerettsegi.py
@@ -0,0 +1,32 @@
+parancs = input("Melyik irányba induljon ez a robot? (észak-dél-kelet-nyugat): ")
+betuk = {}
+e = "E"
+
+for e in parancs:
+    if e in betuk.keys():
+        betuk[e] +=1
+    else:
+        betuk[e] =1
+
+for k,v in betuk.items():
+    print(f"{k} - {v}")
+
+class nehez:
+    def szamolas():
+        if betuk["E"] > betuk["D"]:
+            betuk["E"] - betuk["D"]
+            print(betuk["E"] - betuk["D"])
+        else:
+            betuk["D"] > betuk["E"]
+            betuk["D"] - betuk["E"]
+            print(betuk["D"] - betuk["E"])
+
+        if betuk["N"] > betuk["K"]:
+            betuk["N"] - betuk["K"]
+            print(betuk["N"] - betuk["K"])
+        else:
+            betuk["K"] > betuk["N"]
+            betuk["K"] - betuk["N"]
+            print(betuk["K"] - betuk["N"])
+
+        return ()
